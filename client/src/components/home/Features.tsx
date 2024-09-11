82eb21a7036a651d586f043a65e6ef33df04363c
Nathanim1919 <nathanim2tadele@gmail.com>
2024-09-11 18:51:46 +0300

    add images for features page

--- client/src/components/home/Features.tsx
+++ client/src/components/home/Features.tsx
@@ -1,33 +1,46 @@
 import React from "react";
+import Badge from "../../assets/badge.png"
+import Achievement from "../../assets/achievement.png"
+import Notification from "../../assets/notification.png"
+import Progress from "../../assets/progress.png"
+import trophy from "../../assets/trophy.png"
+import competition from "../../assets/competition.png"
+
 
 const Features: React.FC = () => {
     return (
         <section className="features py-20 bg-gray-100">
-            <div className="container mx-auto px-6">
+            <div className="container mx-auto px-6 w-[80%]">
                 <h2 className="text-center text-4xl font-bold mb-12">Our Features</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     <FeatureBox
+                        image={trophy as string}
                         title={"Customizable Challenges"}
                         description={"Create and personalize challenges that align with your unique goals and interests."}
                     />
                     <FeatureBox
+                          image={competition as string}
                         title={"Real-time Leaderboards"}
                         description={"Stay competitive with dynamic leaderboards that update in real-time as you progress."}
                     />
                     <FeatureBox
+                          image={Achievement as string}
                         title={"Achievement Badges"}
                         description={"Unlock exclusive badges for completing challenges and reaching new milestones."}
                     />
                     <FeatureBox
+                          image={competition as string}
                         title={"Social Sharing"}
                         description={"Share your achievements and challenge completions with your friends on social media."}
                     />
                     <FeatureBox
+                          image={Progress as string}
                         title={"Progress Tracking"}
                         description={"Monitor your progress through detailed statistics and insightful analytics."}
                     />
                     <FeatureBox
-                        title={"Collaborative Challenges"}
+                          image={Notification as string}
+                        title={"Real Time Notification"}
                         description={"Team up with friends or other users to participate in group challenges for more fun."}
                     />
                 </div>
@@ -36,9 +49,12 @@ const Features: React.FC = () => {
     );
 };
 
-const FeatureBox: React.FC<{ title: string; description: string }> = ({ title, description }) => {
+const FeatureBox: React.FC<{ title: string; description: string; image: string }> = ({ title, description, image }) => {
     return (
         <div className="feature-box p-8 bg-white shadow-lg rounded-lg hover:shadow-2xl transition-shadow duration-300">
+            <div className={"w-[130px] relative"}>
+               <img src={image} alt={title} className="w-full h-full relative mx-auto mb-6" />
+            </div>
             <h3 className="text-xl font-bold mb-4">{title}</h3>
             <p className="text-gray-500">{description}</p>
         </div>
--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -3,14 +3,17 @@ import HeroCard from "./HeroCards.tsx";
 
 const Hero: React.FC = () => {
     return (
-        <section className="h-[calc(100vh-4rem)] hero overflow-hidden grid justify-center">
+        <section className="font-Montserrat h-[calc(100vh-4rem)] hero overflow-hidden grid justify-center">
             <div className="hero-content overflow-hidden">
-                <div className={'flex flex-col items-center gap-2 pt-10'}>
-                    <h1 className="hero-title text-5xl md:text-7xl font-bold text-center"> Level Up Your Challenges<br/>and
-                        Achievements </h1>
-                    <p className="hero-text text-center text-gray-500">Join the global platform where you can
-                        challenge yourself, compete, and grow. </p>
-                    <button className="btn-primary">Join Now</button>
+                <div className={'flex flex-col items-center gap-2 pt-16'}>
+                    <h1 className="hero-title text-5xl md:text-6xl lg:text-7xl font-bold text-center"> Discover New Challenges<br/>and
+                        Achieve Greatness </h1>
+                    <p className="hero-text text-center text-gray-500">Join the global platform where you can challenge
+                        yourself, compete, and grow. </p>
+                    <button
+                        className="btn btn-primary py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Join
+                        Now
+                    </button>
                 </div>
                 <div
                     className={'challenge-card-lists absolute overflow-hidden bottom-0 right-0 left-0 flex  justify-center gap-2'}>
