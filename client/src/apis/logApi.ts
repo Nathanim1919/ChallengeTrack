88cf34557cb97c7096a6b93868a67e00c366e239
Nathanim1919 <nathanim2tadele@gmail.com>
2024-10-21 11:32:24 +0300

    configure redux actions and slice for logs

--- client/src/apis/logApi.ts
+++ client/src/apis/logApi.ts
@@ -1,33 +1,34 @@
+import { ApiResponse } from '../interfaces/ICommon';
 import {ILog} from '../interfaces/ILogs';
 import apiConfig from './apiConfig';
 
 
-export const createLog = async (logData: ILog): Promise<ILog> => {
+export const createLog = async (logData: ILog): Promise<ApiResponse<ILog>> => {
     const response = await apiConfig.post('/logs', logData);
     return response.data;
 };
 
-export const getChallengeLogs = async (challengeId: string): Promise<ILog[]> => {
+export const getChallengeLogs = async (challengeId: string): Promise<ApiResponse<ILog[]>> => {
     const response = await apiConfig.get(`/logs/challenge/${challengeId}`);
     return response.data;
 };
 
-export const getUserLogs = async (userId: string): Promise<ILog[]> => {
-    const response = await apiConfig.get(`/logs/${userId}`);
+export const getUserLogs = async (): Promise<ApiResponse<ILog[]>> => {
+    const response = await apiConfig.get(`/logs`);
     return response.data;
 };
 
-export const getChallengeUserLogs = async (challengeId: string): Promise<ILog[]> => {
+export const getChallengeUserLogs = async (challengeId: string): Promise<ApiResponse<ILog[]>> => {
     const response = await apiConfig.get(`/logs/userLog/${challengeId}`);
     return response.data;
 };
 
-export const getLogById = async (logId: string): Promise<ILog | null> => {
+export const getLogById = async (logId: string): Promise<ApiResponse<ILog> | null> => {
     const response = await apiConfig.get(`/logs/${logId}`);
     return response.data;
 };
 
-export const updateLogById = async (logId: string, updateData: Partial<ILog>): Promise<ILog | null> => {
+export const updateLogById = async (logId: string, updateData: Partial<ILog>): Promise<ApiResponse<ILog> | null> => {
     const response = await apiConfig.put(`/logs/${logId}`, updateData);
     return response.data;
 };
--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,4 +1,6 @@
+// Desc: Index file for services
 export * as authServices from "./authService";
 export * as categoryServices from "./categorieService";
 export * as leaderboardService from "./leaderboardService";
 export * as userServices from "./userService";
+export * as logServices from "./log.service";
