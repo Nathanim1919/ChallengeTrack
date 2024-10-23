58985a8a7a59baabc375c3ffb95c67df6c85e523
Nathanim1919 <nathanim2tadele@gmail.com>
2024-10-23 18:41:11 +0300

    modiefy: modify the style for similar challenge cards

--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -9,6 +9,8 @@ import { LuUsers } from "react-icons/lu";
 import { IoLogOutOutline } from "react-icons/io5";
 import { logoutUser } from "../../features/auth/authActions";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { MdOutlineExplore } from "react-icons/md";
+
 
 
 
@@ -21,42 +23,42 @@ const Sidebar: React.FC = () => {
             </div>
             <nav className="justify-self-end flex flex-col justify-between gap-10">
                 <ul className="flex justify-center align-center flex-col gap-5">
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <LuLayoutDashboard />
                        <Link to="/in">Dashboard</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <LuUsers />
                        <Link to="/in/users">Users</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
-                        <BsCollection />
-                        <Link to="/in/challenges">Challenges</Link>
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                        <MdOutlineExplore  className="text-1xl"/>
+                        <Link to="/in/challenges">Explore</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <BsCollection />
                         <Link to="/in/my-challenges">My Challenges</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <MdOutlineLeaderboard />
                         <Link to="/in/leaderboard">LeaderBoard</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <MdOutlineLeaderboard />
                         <Link to="/in/categories">Categories</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <IoNotificationsOutline />
                         <Link to="/in/notifications">Notification</Link>
                     </li>
-                    <li className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <IoSettingsOutline />
                         <Link to="/in/settings">Settings</Link>
                     </li>
                 
                 </ul>
                 <ul>
-                    <li onClick={() => dispatch(logoutUser())} className="flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
+                    <li onClick={() => dispatch(logoutUser())} className="font-bold flex items-center gap-3 p-2 text-[#333] hover:text-black hover:bg-gray-200 border border-transparent hover:border hover:border-gray-200 cursor-pointer transform transition-all duration-200 text-[15px]">
                         <IoLogOutOutline />
                         <Link to="/login">Logout</Link>
                     </li>
--- client/src/pages/Challenges/SimilarChallenges.tsx
+++ client/src/pages/Challenges/SimilarChallenges.tsx
@@ -47,15 +47,98 @@ const SimilarChallenges = () => {
         duration:"30 days",
         date:"Mon Sep 30 2024 - Mon Oct 30 2024",
         participants:40,
-       }
+       },
+       {
+        title: "30 days fitness challenge",
+        category: "fitness",
+        duration: "30 days",
+        date: "Mon Sep 30 2024 - Mon Oct 30 2024",
+        participants: 40,
+        onTheRightTrack: 30,
+        needToCatchUp: 10,
+        leave: 2
+    },
+   {
+      title:"Weight Loss Challenge",
+      category:"fitness",
+      duration:"30 days",
+      date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+      participants:40,
+      onTheRightTrack:30,
+      needToCatchUp:10,
+      leave:2
+   },
+   {
+    title:"Learn React Challenge",
+    category:"programming",
+    duration:"30 days",
+    date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+    participants:40,
+   },
+   {
+    title:"Learn Go Challenge",
+    category:"programming",
+    duration:"30 days",
+    date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+    participants:40,
+   },
+   {
+    title:"Learn Node Challenge",
+    category:"programming",
+    duration:"30 days",
+    date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+    participants:40,
+   },
+   {
+    title: "30 days fitness challenge",
+    category: "fitness",
+    duration: "30 days",
+    date: "Mon Sep 30 2024 - Mon Oct 30 2024",
+    participants: 40,
+    onTheRightTrack: 30,
+    needToCatchUp: 10,
+    leave: 2
+},
+{
+  title:"Weight Loss Challenge",
+  category:"fitness",
+  duration:"30 days",
+  date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+  participants:40,
+  onTheRightTrack:30,
+  needToCatchUp:10,
+  leave:2
+},
+{
+title:"Learn React Challenge",
+category:"programming",
+duration:"30 days",
+date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+participants:40,
+},
+{
+title:"Learn Go Challenge",
+category:"programming",
+duration:"30 days",
+date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+participants:40,
+},
+{
+title:"Learn Node Challenge",
+category:"programming",
+duration:"30 days",
+date:"Mon Sep 30 2024 - Mon Oct 30 2024",
+participants:40,
+}
     ];
     return ( 
-        <div className="bg-[#eee] border-r border-gray-200">
+        <div className="bg-[#eee] border-r border-gray-200 overflow-hidden">
           <h1 className="font-bold p-2 py-4 bg-white flex items-center gap-2"><BsCollection/>You may also like</h1>
-          <div className="grid gap-1 p-2">
-            {loading? <div className=' grid place-items-center py-5'><ButtonLoading/></div>:
+          <div className="grid gap-1 p-2 overflow-y-auto h-[85vh]">
+            {loading? 
+            <div className=' grid place-items-center py-5'><ButtonLoading/></div>:
               challenges.map((challenge, index) => (
-                <div key={index} className="border-transparent flex justify-between items-center px-2 py-1  cursor-pointer border hover:border-gray-200 bg-white hover:bg-gray-100">
+                <div key={index} className="border-gray-300 flex justify-between items-center px-2 py-1  cursor-pointer border hover:border-gray-200 bg-white hover:bg-gray-100">
                    <div className="flex items-center gap-2">
                     <div className="w-10 h-10 bg-gray-200 rounded-md"></div>
                     <div className="flex flex-col items-start">
